869c61e4bc0c6d2f59f893a69c6c84de410966b7
kento sugimoto <kento1109@gmail.com>
2021-02-13 15:09:43 +0900

    minor update

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from radie.src.utils.ner_utils import Tagger
 # from radie.src.utils.re_utils import Rex
-# from radie.src.utils.writer import MlflowWriter
+from radie.src.utils.writer import MlflowWriter
